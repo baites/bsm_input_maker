70689c2b72c31d927be2c710c7d0d1fd66e96dba
Samvel Khalatyan <ksamdev@gmail.com>
2011-04-19 15:45:19 -0500

    Randomize event in the write example

--- src/write.cpp
+++ src/write.cpp
@@ -8,11 +8,19 @@
 #include <iostream>
 
 #include <boost/filesystem.hpp>
+#include <boost/lexical_cast.hpp>
+
+#include <TRandom3.h>
 
 #include "interface/Event.pb.h"
 #include "interface/Writer.h"
 
 using bsm::Event;
+using bsm::Jet;
+using bsm::LorentzVector;
+using bsm::Muon;
+using bsm::PrimaryVertex;
+using bsm::Vector;
 using bsm::Writer;
 
 using std::cerr;
@@ -24,30 +32,95 @@ namespace fs = boost::filesystem;
 int main(int argc, const char *argv[])
 try
 {
+    GOOGLE_PROTOBUF_VERIFY_VERSION;
+
     if (2 > argc)
     {
-        cerr << "Usage: " << argv[0] << " output.pb" << endl;
+        cerr << "Usage: " << argv[0] << " [events] output.pb" << endl;
+
+        google::protobuf::ShutdownProtobufLibrary();
 
         return 1;
     }
 
     {
-        boost::shared_ptr<Writer> writer(new Writer(fs::path(argv[1])));
-        boost::shared_ptr<Event> event(new Event());
+        int events = 100;
+        boost::shared_ptr<Writer> writer;
 
-        const int events = 100;
+        if (3 == argc)
+        {
+            events = boost::lexical_cast<uint32_t>(argv[1]);
+            writer.reset(new Writer(fs::path(argv[2])));
+        }
+        else
+            writer.reset(new Writer(fs::path(argv[1])));
+
+        boost::shared_ptr<Event> event(new Event());
+        boost::shared_ptr<TRandom> randomizer(new TRandom3());
+        randomizer->SetSeed(time(0));
 
         cout << "Generate: " << events << " events" << endl;
 
         for(int i = 0; events > i; ++i)
         {
+            for(int pv = 0, pvs = randomizer->Poisson(2); pvs > pv; ++pv)
+            {
+                PrimaryVertex *pv = event->add_primary_vertices();
+
+                Vector *vertex = pv->mutable_vertex();
+
+                vertex->set_x(randomizer->Gaus(0, .3));
+                vertex->set_y(randomizer->Gaus(0, .5));
+                vertex->set_z(randomizer->Gaus(0, .7));
+            }
+
+            for(int j = 0, jets = randomizer->Poisson(5); jets > j; ++j)
+            {
+                Jet *jet = event->add_jets();
+                LorentzVector *p4 = jet->mutable_physics_object()->mutable_p4();
+
+                p4->set_e(randomizer->Gaus(172, 10));
+                p4->set_px(randomizer->Gaus(100, 15));
+                p4->set_py(randomizer->Gaus(100, 20));
+                p4->set_pz(randomizer->Gaus(100, 25));
+
+                Vector *vertex = jet->mutable_physics_object()->mutable_vertex();
+
+                vertex->set_x(randomizer->Gaus(0, 5));
+                vertex->set_y(randomizer->Gaus(0, 4));
+                vertex->set_z(randomizer->Gaus(0, 3));
+            }
+
+            for(int m = 0, muons = randomizer->Poisson(10); muons > m; ++m)
+            {
+                Muon *muon = event->add_muons();
+                LorentzVector *p4 = muon->mutable_physics_object()->mutable_p4();
+
+                p4->set_e(randomizer->Gaus(.105, 0.005));
+                p4->set_px(randomizer->Gaus(10, 3));
+                p4->set_py(randomizer->Gaus(10, 2));
+                p4->set_pz(randomizer->Gaus(10, 1));
+
+                Vector *vertex = muon->mutable_physics_object()->mutable_vertex();
+
+                vertex->set_x(randomizer->Gaus(1, 2));
+                vertex->set_y(randomizer->Gaus(1, 3));
+                vertex->set_z(randomizer->Gaus(1, 4));
+            }
+
             writer->write(*event);
+
+            event->Clear();
         }
     }
+
+    google::protobuf::ShutdownProtobufLibrary();
 
     return 0;
 }
 catch(...)
 {
     cerr << "Unknown error" << endl;
+
+    google::protobuf::ShutdownProtobufLibrary();
 }
