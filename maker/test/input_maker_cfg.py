b70debab45f16dff27f66ca3ec619959c152007a
Samvel Khalatyan <samvel.khalatyan@gmail.com>
2011-04-20 13:07:05 -0500

    Add testing input_maker config

---
+++ maker/test/input_maker_cfg.py
@@ -0,0 +1,50 @@
+import os
+
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process("MCInputMaker")
+
+#-- Message Logger ------------------------------------------------------------
+process.load("FWCore.MessageService.MessageLogger_cfi")
+process.MessageLogger.categories.append('PATSummaryTables')
+process.MessageLogger.cerr = cms.untracked.PSet(
+    default          = cms.untracked.PSet(limit = cms.untracked.int32(-1)),
+    PATSummaryTables = cms.untracked.PSet(limit = cms.untracked.int32(-1)),
+    FwkReport = cms.untracked.PSet(
+        reportEvery = cms.untracked.int32(1000)
+    )
+)
+
+#-- Input Source --------------------------------------------------------------
+process.source = cms.Source("PoolSource",
+    fileNames = cms.untracked.vstring()
+)
+
+if (os.path.exists("input.txt")):
+    for file in open("input.txt").readlines():
+        process.source.fileNames.append("dcap:///pnfs/cms/WAX/11" + file.strip())
+
+process.maxEvents = cms.untracked.PSet(
+        input = cms.untracked.int32(1000)
+)
+
+process.options = cms.untracked.PSet(
+    wantSummary = cms.untracked.bool(True)
+)
+
+#-- TFileService --------------------------------------------------------------
+process.TFileService = cms.Service(
+    "TFileService",
+    fileName = cms.string("mc_kinematics.root"),
+    closeFileFast = cms.untracked.bool(True)
+)
+
+#-- Analysis ------------------------------------------------------------------
+process.load("bsm_input_maker.maker.InputMaker_cfi")
+
+process.p = cms.Path(
+    process.InputMaker
+)
+
+import FWCore.ParameterSet.printPaths as pp      
+pp.printPaths(process)
