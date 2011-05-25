56421968f6809b8cb14f2a548ea9a074521873f9
Samvel Khalatyan <samvel.khalatyan@gmail.com>
2011-05-25 16:03:54 -0500

    Forward bsm_input declarations

---
+++ interface/bsm_input_fwd.h
@@ -0,0 +1,15 @@
+// Forward widely-used declarations
+//
+// Created by Samvel Khalatyan, May 20, 2011
+// Copyright 2011, All rights reserved
+
+#ifndef BSM_INPUT_FWD
+#define BSM_INPUT_FWD
+
+namespace bsm
+{
+    class Reader;
+    class Writer;
+}
+
+#endif
