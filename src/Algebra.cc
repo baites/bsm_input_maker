9ebbc496bae17db03eff9a9726da18c05bf2e0c4
Samvel Khalatyan <samvel.khalatyan@gmail.com>
2011-05-29 23:02:51 -0500

    fix Lorentz Vectors subtraction energy component

--- src/Algebra.cc
+++ src/Algebra.cc
@@ -23,7 +23,12 @@ LorentzVector &bsm::operator +=(LorentzVector &v1, const LorentzVector &v2)
 
 LorentzVector &bsm::operator -=(LorentzVector &v1, const LorentzVector &v2)
 {
-    v1.set_e(v1.e() - v2.e());
+    double energy = v1.e() - v2.e();
+    if (0 >= energy)
+        v1.set_e(0);
+    else
+        v1.set_e(energy);
+
     v1.set_px(v1.px() - v2.px());
     v1.set_py(v1.py() - v2.py());
     v1.set_pz(v1.pz() - v2.pz());
@@ -61,7 +66,7 @@ double bsm::mass(const LorentzVector &v)
 {
     double magnitude = v * v;
 
-    return 0 >= magnitude
+    return (0 >= magnitude)
         ? 0
         : sqrt(magnitude);
 }
